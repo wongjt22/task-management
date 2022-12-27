56ed2ed960590bed460ddabc712809061dd909bf
Jackson Wong <wongjt@berkeley.edu>
2022-12-26 22:43:28 -1000

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+
+function App() {
+  return (
+    <div>
+      Task Management App
+    </div>
+  );
+}
+
+export default App;
