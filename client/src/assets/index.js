3ff2b008000b0583639e3b2515870718876b43c1
Jackson Wong <wongjt@berkeley.edu>
2022-12-26 22:41:48 -1000

    light icon, favicon, secondary color

---
+++ client/src/assets/index.js
@@ -0,0 +1,13 @@
+const assets = {
+  images: {
+    logoLight: require('./images/taskflow-logo-light.png'),
+    appIcon: require('./images/taskflow-favicon.png')
+  },
+  colors: {
+    secondary: '#292929'
+  }
+}
+
+export default assets
+
+// TODO - add dark logo
